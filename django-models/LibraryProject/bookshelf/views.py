645df7582d2f7e51695b8bb14df0078fecb80e98
gladysuchola <ucholagladys@gmail.com>
2025-11-06 00:08:12 +0400

    Another commit message for URL configuration

--- django-models/LibraryProject/bookshelf/views.py
+++ django-models/LibraryProject/bookshelf/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.views.generic.detail import DetailView
+from .models import Library
 
-# Create your views here.
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
