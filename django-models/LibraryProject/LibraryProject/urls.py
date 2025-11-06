655e3dbd6d4eeeb76dcb1284fe75769970b9e3f8
gladysuchola <ucholagladys@gmail.com>
2025-11-06 14:48:01 +0400

    4th commit message for URL Config

--- django-models/LibraryProject/LibraryProject/urls.py
+++ django-models/LibraryProject/LibraryProject/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path
-from .views import list_books, LibraryDetailView
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('books/', list_books, name='list_books'),
-    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+    path('admin/', admin.site.urls),
+    path('', include('relationship_app.urls')),  # ✅ This line includes your app's URLs
 ]
+
+
