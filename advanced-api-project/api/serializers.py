1b1b047fe207b3117df6fd89b21e194d530f41b6
gladysuchola <ucholagladys@gmail.com>
2025-11-28 11:19:12 +0400

    Update for Book Serializer

--- advanced-api-project/api/serializers.py
+++ advanced-api-project/api/serializers.py
@@ -3,6 +3,10 @@ from .models import Author, Book
 import datetime
 
 class BookSerializer(serializers.Modelserializer):
+    """
+    Serializer for the Book model.
+    Includes validation to ensure publication_year is not in the future.
+    """
     class Meta:
         model = Book
         fields = ['id', 'title', 'publication_year', 'author']
@@ -15,7 +19,10 @@ class BookSerializer(serializers.Modelserializer):
 
     class AuthorSerializer(serializers.ModelSerializer):
         books = BookSerializer(many=True, read_only=True)
-
+        """
+        Serializer for the Author model.
+        Nests BookSerializer to show all books written by the author.
+        """
         class Meta:
             model = Author
             fields = ['id', 'name', 'books']   
