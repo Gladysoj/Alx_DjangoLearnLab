2244ac888f34da16ea4323bc5c7baa2d31387e06
gladysuchola <ucholagladys@gmail.com>
2025-12-03 13:22:43 +0400

    Blog User Authentication

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import AuthenticationForm
+from .forms import CustomUserCreationForm
 
-# Create your views here.
+def register_view(request):
+    if request.method == "POST":
+         form = CustomUserCreationForm(request.POST)
+         if form.is_valid():
+            user = form.save()
+            login(request, user)  # auto-login after registration
+            return redirect("profile")
+    else:
+        form = CustomUserCreationForm()
+    return render(request, "register.html", {"form": form})
+
+# Login
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect("profile")
+    else:
+        form = AuthenticationForm()
+    return render(request, "login.html", {"form": form})
+
+# Logout
+def logout_view(request):
+    logout(request)
+    return redirect("login")
+
+# Profile
+def profile_view(request):
+    if request.method == "POST":
+        request.user.email = request.POST.get("email")
+        request.user.save()
+    return render(request, "profile.html", {"user": request.user})
+
+
--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -77,12 +77,8 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'myblogdb',
-        'USER': 'myusername',
-        'PASSWORD': 'mypassword',
-        'HOST': 'localhost',   # or your DB server address
-        'PORT': '5432',        # default PostgreSQL port
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
     }
 }
 
